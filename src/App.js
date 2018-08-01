10b0b44e065460f3a811adde548e043cc3a1af9b
abimaelhernandez <hernandezabimael364@gmail.com>
2018-07-31 19:07:50 -0500

    numbers are now being mutated correctly

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      list : [],
+      list : []
     }
   }
 
   //initial state for0 the listl
   componentDidMount(){
-    // this.setState({
-    //   list: employees
-    // })
+
     this.setState((state) => {
       //updatew salary in employee obj
-      employees.map(obj => {
-        return this.numToMoney(obj.salary)
+      employees.forEach((employee, i ) => {
+
+        let result = this.numToMoney(employee.salary)
         //this.numToMoney(employees.salary)
         //console.log('hola money',obj.salary)
         //set the employees prop on state to employeen obj
+        employees[i].salary = result
         //return state
       })
+      state.list = employees
+      return state
     })
+
   }
 
   //function for convert num to money
@@ -49,50 +52,49 @@ class App extends Component {
 //numToMoney(16900)
 
 //this deletes the user by using id of them
-// deleteUser = (item) => {
-//   console.log('this is the id',item.id);
-//   let {list} = this.state
-//   list.splice(item.id,1)
-//   this.setState({
-//     list : list
-//   })
-//    console.log('help me delete!!!', this.state.list);
-// }
+deleteUser = (item) => {
+  console.log('this is the id',item.id);
+  let {list} = this.state
+  list.splice(item.id,1)
+  this.setState({
+    list : list
+  })
+   console.log('help me delete!!!', this.state.list);
+}
 
   render() {
     return (
       <div id='employees'> Hello world
+        <table>
+        <tbody>
+          <tr>
+            <th>Age</th>
+            <th>Company</th>
+            <th>Email</th>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Salary</th>
+          </tr>
+          {this.state.list.map(item => {
+            return (
+          <tr key={item.id}>
+              <td>{item.age}</td>
+              <td>{item.company}</td>
+              <td>{item.email}</td>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.phone}</td>
+              <td>{item.salary}</td>
+              <a className="delete" onClick={this.deleteUser.bind(this,item)} href='#'>X</a>
+          </tr>
+    )
+    })}
+  </tbody>
+  </table>
       </div>
     )
   }
 }
 
 export default App;
-
-//<table>
-  // <tbody>
-  //   <tr>
-  //     <th>Age</th>
-  //     <th>Company</th>
-  //     <th>Email</th>
-  //     <th>Id</th>
-  //     <th>Name</th>
-  //     <th>Phone</th>
-  //     <th>Salary</th>
-  //   </tr>
-  //   {this.state.list.map(item => {
-  //     return (
-  //     <tr key={item.id}>
-  //       <td>{item.age}</td>
-  //       <td>{item.company}</td>
-  //       <td>{item.email}</td>
-  //       <td>{item.id}</td>
-  //       <td>{item.name}</td>
-  //       <td>{item.phone}</td>
-  //       <td>{item.salary}</td>
-  //       <a className="delete" onClick={this.deleteUser.bind(this,item)} href='#'>X</a>
-  //     </tr>
-  //   )
-  //   })}
-  // </tbody>
-//</table>
